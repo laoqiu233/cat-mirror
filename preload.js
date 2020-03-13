17d0a12897ac943241df3db446ffcc3f5556f6b6
Dmitri Qiu <dmitrimeowsalot@gmail.com>
2020-03-14 08:13:03 +1000

    [ADD] Added font resize

--- preload.js
+++ preload.js
@@ -1,8 +1,22 @@
 const {remote} = require('electron')
 
 document.addEventListener('keydown', e => {
-    if (e.key == 'Escape') {
-        remote.getCurrentWindow().close();
+    switch (e.key) {
+        case 'Escape':
+            remote.getCurrentWindow().close();
+            break;
+        case 'ArrowUp':
+            docSize = document.documentElement.style.fontSize;
+            currentSize = docSize ? parseInt(docSize) : 10;
+            document.documentElement.style.fontSize = `${currentSize+1}px`;
+            break;
+        case 'ArrowDown':
+            docSize = document.documentElement.style.fontSize;
+            currentSize = docSize ? parseInt(docSize) : 10;
+            document.documentElement.style.fontSize = `${currentSize-1}px`;
+            break;
+        default:
+            break;
     }
 });
 
